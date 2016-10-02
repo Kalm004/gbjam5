969e7cd83382f01a41dc2bf523800c841155a0e2
Kalm004 <kalm004@gmail.com>
2016-10-02 23:04:20 +0100

    First version with fixed resolution

---
+++ Assets/FixScreenResolution.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixScreenResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(160, 144, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
