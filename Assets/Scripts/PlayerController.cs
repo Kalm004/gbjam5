78e54d702810e3c5f1373915fb814dd0f621dd2d
Kalm004 <kalm004@gmail.com>
2016-10-03 22:55:23 +0100

    First version of the gameplay.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velocity = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity = -5;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
+    }
+
+    public void FixedUpdate()
+    {
+        Physics2D.IgnoreLayerCollision(gameObject.layer, platformPrefab.layer, rb.velocity.y > 0);
+    }
+}
