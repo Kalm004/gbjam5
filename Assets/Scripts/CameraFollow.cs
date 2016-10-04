fb40fcaf860195b7cba672edd937d4013459b9a7
Kalm004 <kalm004@gmail.com>
2016-10-04 23:08:11 +0100

    First version of the camera movement.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Rigidbody2D playerRb;
+    private bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!moving)
+        {
+            if (playerRb.velocity.y == 0 && Camera.main.WorldToScreenPoint(playerRb.transform.position).y > 100)
+            {
+                moving = true;
+            }
+        } else
+        {
+            transform.Translate(0, 5 * Time.fixedDeltaTime, 0);
+            if (Camera.main.WorldToScreenPoint(playerRb.transform.position).y < 40)
+            {
+                moving = false;
+            }
+        }
+	}
+}
