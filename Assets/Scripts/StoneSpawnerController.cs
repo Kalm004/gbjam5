bc76da319b644bd10d1c7ece4ffd1caeda3ffe79
Kalm004 <kalm004@gmail.com>
2016-10-06 19:41:01 +0100

    Spawn rocks on trigger enter.

---
+++ Assets/Scripts/StoneSpawnerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneSpawnerController : MonoBehaviour {
+    public GameObject rockPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        stayUpOnScreen();
+    }
+
+    public void generateStone()
+    {
+        GameObject rock = GameObject.Instantiate(rockPrefab);
+        rock.transform.position = transform.position;
+    }
+
+    private void stayUpOnScreen()
+    {
+
+    }
+}
---
+++ Assets/Scripts/StoneTriggerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneTriggerController : MonoBehaviour
+{
+    public StoneSpawnerController[] spawners;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == GameTags.PlayerBig)
+        {
+            foreach (StoneSpawnerController spawnerCtrl in spawners)
+            {
+                spawnerCtrl.generateStone();
+            }
+        }
+    }
+}
