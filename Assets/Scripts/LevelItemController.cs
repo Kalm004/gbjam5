9ff7ac08837084116c845de29de6594496f899f4
Kalm004 <kalm004@gmal.com>
2016-10-09 00:27:57 +0100

    Basic version of the level selection.

---
+++ Assets/Scripts/LevelItemController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelItemController : MonoBehaviour {
+    public Image[] gems;
+    public Text levelNumberText;
+    public GameObject selector;
+    public float blinkTime = 0.1f;
+    private bool selected = true;
+    private float timeSinceLastBlink = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (selected)
+        {
+            timeSinceLastBlink += Time.deltaTime;
+            if (timeSinceLastBlink > blinkTime)
+            {
+                timeSinceLastBlink = 0;
+                selector.SetActive(!selector.activeSelf);
+            }
+        } else
+        {
+            selector.SetActive(false);
+        }
+	}
+}
---
+++ Assets/Scripts/LevelSelectionController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionController : MonoBehaviour {
+    private int selectedLevel = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene(Scenes.Level + selectedLevel.ToString());
+        }
+	}
+}
